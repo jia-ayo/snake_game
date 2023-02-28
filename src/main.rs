85a6c6f179affcceb260135604349c47b1095501
jia-ayo <milkjia11@gmail.com>
2023-02-28 21:20:19 +0100

    primitive type 3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
-    //_ => formating purpose for readability
-    let custom_num = 98_000;//98000
-    let hex_num= 0xfa;//250
-    let bin_num = 0b0010_1011;//43
-    let byte_num = b'A';//  
-    println!("{}", custom_num);
-    println!("{}", hex_num);
-    println!("{}", bin_num);
-    println!("{}", byte_num);
+    //FLOATS
+    let float_num: f32 = 3.14;
+    let float_nume_2: f64 = 3.234343545;
+
+    //TURPLE
+    let tup: (i32, f64, &str) = (20, 3.12, "hello");
+
+    println!("{}", tup.2);
+
+    let (a, b, c) = tup;
+    println!("{}", a);
+    //ARRAY
+    let x = [1, 5, 6, 7];
+    println!("{}", x[2]);
+
+    let y = [2; 6]; //[2,2,2,2,2,2]
+    println!("{}", y[5]);
 }
