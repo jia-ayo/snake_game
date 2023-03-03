379bded1c0c617a2bac2cd2cf47349df80f9707e
jia-ayo <milkjia11@gmail.com>
2023-03-03 09:14:32 +0100

    how it is moved

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 fn main(){
-   let message = String::from("Hello");//message comming into the scope
-   print_message(message);//message is moved into print messsage function
-   //message is no longer valide
+    let  message = String::from("Hello");
+    let message_2 = extend_message(message);
+    println!("{}", message_2)
 }
-//message is going out of the scope
-//but nothing more will happen because it was moved into print_message
 
-fn print_message(a: String) {//a comes into the scope
-    println!("{}", a);
-    let c = a;//c is coming into the scope and a is moved into c 
-    //a is no longer valid
+fn extend_message(mut a: String)->String{
+    a.push_str(" world");
+    a 
 }
-//a is going out of the scope, but nothing more will happen because it was moved
-//c is going out of the scope and "drop"
-//is called wich clears the memory from the heap
