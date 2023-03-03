04d16efb0e2d2031db09f376cb8902d5f69b8c9d
jia-ayo <milkjia11@gmail.com>
2023-03-03 09:19:35 +0100

    copingand moving

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 fn main(){
     let  message = String::from("Hello");
-    let message_2 = extend_message(message);
+    let message_2 =  extend_message(message);
+    
+    let age = 30;
+    extend_age(age);
+    println!("{}", age);
+
     println!("{}", message_2)
 }
 
 fn extend_message(mut a: String)->String{
     a.push_str(" world");
     a 
+}
+
+fn extend_age(mut a: u32) -> u32{
+    a += 100;
+    a
 }
