376346aa4ef60bd272f25220bf2006d3d47624ea
jia-ayo <milkjia11@gmail.com>
2023-03-05 07:42:30 +0100

    mutable and immutable referrence

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 fn main(){
     let mut message = String::from("Hello");
+    let message_3= &message;
+    println!("{}", message_3);
     let message_2: &mut String =  &mut message;
 
-    //message_2 is not owner of data
-    //message_2 is "borrowing" a reference to message
+   
+    unpredictable_mutate(message_2);
     
-    message_2.push_str(" world");
     
-    println!("{}", message_2);
     println!("{}", message);
 }
 
-// message and message_2 going
-// message_2 is not dropped because it doesnt hve ownership of what it refers to
-//message is dropped
+fn unpredictable_mutate(val:&mut String){
+    val.push_str("_unpredictable")
+}
