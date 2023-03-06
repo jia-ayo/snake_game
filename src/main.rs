e1e921cef026ea7befed5abf74b53d4cc39ef15c
jia-ayo <milkjia11@gmail.com>
2023-03-06 10:29:38 +0100

    clone

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 fn main(){
     let mut message = String::from("Hello");
-    let slice = &message[2..4];//2 -> 3
-    //H E L L O
-    //0 1 2 3 4
 
-    println!("{}", slice);
+    //this creates a new instance in the heap 
+    let message_3 = message.clone();
+
+    message.clear();
+
+    println!("{}", message);
+    println!("{}", message_3);
 }
 
