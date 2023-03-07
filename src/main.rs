037b63841108d6707cc44926e1fc4790b6e2c9e4
jia-ayo <milkjia11@gmail.com>
2023-03-07 08:15:09 +0100

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+struct Person{
+    name: String,
+    last_name: String,
+    age: u32
+}
 fn main(){
-    let num = 32;
-    let num_2 = Box::new(num);
-    println!("{}", num_2)
+    let person= Person{
+        name: "jia".to_string(),
+        last_name: "ayo".to_string(),
+        age:20
+    };
+    println!("{} {} is {}", person.last_name, person.name, person.age)
 }
 
