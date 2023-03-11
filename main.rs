46cafc28728920d8b9fc164e01f1530fc6256032
jia-ayo <milkjia11@gmail.com>
2023-03-11 07:59:23 +0100

    import option

--- main.rs
+++ main.rs
@@ -1,11 +1,9 @@
-use snake_game::Person;
-use snake_game::Animal;
-use  snake_game::log_info;
-use  snake_game::log_info_2;
+use snake_game::{Person, Animal, log_info, log_info_2};
+
+//use snake_game::*;
 fn main(){
     let mut person= Person::new();
     let animal = Animal("dog".to_string(), 10,"bulldog".to_string());
-    ]
 
     person.change_age(32);
     
