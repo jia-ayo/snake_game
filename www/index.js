be8f3f47307f9e1768789e4f56f668597c848ff1
jia-ayo <milkjia11@gmail.com>
2023-03-19 09:46:16 +0100

    import wasm

--- www/index.js
+++ www/index.js
@@ -1,8 +1,19 @@
 async function init() {
+  const importObject = {
+    console: {
+      log: () => {
+        console.log("just logging something");
+      },
+      error: () => {
+        console.log("i am just an error");
+      },
+    },
+  };
+
   const response = await fetch("sum.wasm");
   const buffer = await response.arrayBuffer();
 
-  const wasm = await WebAssembly.instantiate(buffer);
+  const wasm = await WebAssembly.instantiate(buffer, importObject);
   const sumfunction = wasm.instance.exports.sum;
   const result = sumfunction(10, 50);
   console.log(result);
