6640f2e26dadc284c75acd155ddeadef11fb3159
jia-ayo <milkjia11@gmail.com>
2023-03-21 21:33:46 +0100

    js memory

--- www/index.js
+++ www/index.js
@@ -1,5 +1,9 @@
 async function init() {
+  const memory = new WebAssembly.Memory({ initial: 1 });
   const importObject = {
+    js: {
+      mem: memory,
+    },
     console: {
       log: () => {
         console.log("just logging something");
@@ -14,12 +18,10 @@ async function init() {
   const buffer = await response.arrayBuffer();
 
   const wasm = await WebAssembly.instantiate(buffer, importObject);
-  const sumfunction = wasm.instance.exports.sum;
-  const wasmMemory = wasm.instance.exports.mem;
-  const unit8Array = new Uint8Array(wasmMemory.buffer, 0, 2);
+  const unit8Array = new Uint8Array(memory.buffer, 0, 2);
 
   const hiText = new TextDecoder().decode(unit8Array);
-  console.log(hiText)
+  console.log(hiText);
   // const result = sumfunction(10, 50);
   // console.log(result);
 }
