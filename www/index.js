e5349bad52309eedcfae124b48e24ab91b68d5db
jia-ayo <milkjia11@gmail.com>
2023-03-22 19:29:05 +0100

    init wasm js

--- www/index.js
+++ www/index.js
@@ -1,28 +1,6 @@
-async function init() {
-  const memory = new WebAssembly.Memory({ initial: 1 });
-  const importObject = {
-    js: {
-      mem: memory,
-    },
-    console: {
-      log: () => {
-        console.log("just logging something");
-      },
-      error: () => {
-        console.log("i am just an error");
-      },
-    },
-  };
+import init, { greet } from "snake_game";
 
-  const response = await fetch("sum.wasm");
-  const buffer = await response.arrayBuffer();
-
-  const wasm = await WebAssembly.instantiate(buffer, importObject);
-  const unit8Array = new Uint8Array(memory.buffer, 0, 2);
-
-  const hiText = new TextDecoder().decode(unit8Array);
-  console.log(hiText);
-  // const result = sumfunction(10, 50);
-  // console.log(result);
-}
-init();
+init().then(_ => {
+  greet("jia");
+  console.log("ok")
+})
