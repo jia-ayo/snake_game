17f2f51049bd9043f52abc99d9cadd3ba776a50c
jia-ayo <milkjia11@gmail.com>
2023-03-26 16:56:44 +0100

    direction left and right

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,24 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
+#[derive(PartialEq)]
 enum Direction{
-    
+    Up,
+    Right,
+    Down,
+    Left,
 }
 
 struct SnakeCell(usize);
 struct Snake {
     body: Vec<SnakeCell>,
+    direction: Direction
 }
 impl Snake {
     fn new(spawn_index: usize) -> Snake {
         Snake {
             body: vec![SnakeCell(spawn_index)],
+            direction: Direction::Left,
         }
     }
 }
@@ -43,7 +49,15 @@ impl World {
 
     pub fn update(&mut self) {
         let snake_idx = self.snake_head_idx();
-        self.snake.body[0].0 = (snake_idx + 1) % self.size;
+        
+
+        if self.snake.direction == Direction::Right{
+            self.snake.body[0].0 = (snake_idx + 1) % self.size;
+        }
+        if self.snake.direction == Direction::Left{
+            self.snake.body[0].0 = (snake_idx - 1) % self.size;
+        }
+
     }
 }
 
